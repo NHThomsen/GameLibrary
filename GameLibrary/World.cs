4ed1e82f81fb1f965067aaa4d6ed35e4efb05cf6
nhthomsen <nooblas@hotmail.com>
2024-03-12 10:42:15 +0100

    Add project files.

---
+++ GameLibrary/World.cs
@@ -0,0 +1,48 @@
+﻿using System.Xml;
+
+namespace GameLibrary
+{
+    public class World
+    {
+        public string Name { get;  private set; }
+        public int WorldLength { get; private set; }
+        private int length;
+        public int WorldHeight { get; private set; }
+        private int height;
+        private XmlDocument? configDocument = new XmlDocument();
+
+        public World()
+        {
+            configDocument.Load("WorldConfig.xml");
+            XmlNode? nameNode = configDocument.DocumentElement.SelectSingleNode("Name");
+            if (nameNode != null)
+            {
+                Name = nameNode.InnerText.Trim();
+            }
+            else
+            {
+                Name = "Standard world";
+            }
+
+            XmlNode lengthNode = configDocument.DocumentElement.SelectSingleNode("Length");
+            if (int.TryParse(lengthNode.InnerText.Trim(), out length))
+            {
+                WorldLength = length;
+            }
+            else
+            {
+                WorldLength = 10;
+            }
+
+            XmlNode heightNode = configDocument.DocumentElement.SelectSingleNode("Height");
+            if(int.TryParse(heightNode.InnerText.Trim(),out height))
+            {
+                WorldHeight = height;
+            }
+            else
+            {
+                WorldHeight = 10;
+            }
+        }
+    }
+}
---
+++ GameLibraryTests/WorldTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GameLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLibrary.Tests
+{
+    [TestClass()]
+    public class WorldTests
+    {
+        [TestMethod()]
+        public void TestNameFromConfig()
+        {
+            string expectedName = "Far far away";
+
+            World world = new World();
+
+            string actualName = world.Name;
+
+            Assert.AreEqual(expectedName, actualName);
+        }
+
+        public void TestLengthFromConfig() 
+        { }
+    }
+}
