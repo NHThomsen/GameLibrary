055570dca9d254d56d228f9f287a95bbab6c8dab
nhthomsen <nooblas@hotmail.com>
2024-03-12 12:51:50 +0100

    Added further classes

---
+++ GameLibrary/Classes/Creatures/Creature.cs
@@ -0,0 +1,18 @@
+﻿using GameLibrary.Classes.World;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLibrary.Classes.Creatures
+{
+    public class Creature
+    {
+        public Position Position;
+        public Creature(Position position) 
+        {
+            Position = position;
+        }
+    }
+}
---
+++ GameLibrary/Classes/Items/Item.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLibrary.Classes.Items
+{
+    public abstract class Item
+    {
+        public string Name { get; protected set; }
+        public string Description { get; protected set; }
+
+        public Item(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+    }
+}
