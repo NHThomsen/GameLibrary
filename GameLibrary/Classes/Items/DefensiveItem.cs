8c733c62bed7b4cf7b449f605e5ae42b4680225f
nhthomsen <nooblas@hotmail.com>
2024-03-19 13:33:29 +0100

    Added more item classes

---
+++ GameLibrary/Classes/Items/DefensiveItem.cs
@@ -0,0 +1,18 @@
+﻿using GameLibrary.Classes.Damage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLibrary.Classes.Items
+{
+    public class DefensiveItem : Item
+    {
+        public DamageReduction DamageReduction { get; set; }
+        public DefensiveItem(string name, string description, DamageReduction damageReduction) : base(name, description) 
+        {
+            DamageReduction = damageReduction;
+        }
+    }
+}
---
+++ GameLibrary/Classes/Items/OffensiveItem.cs
@@ -0,0 +1,21 @@
+﻿using GameLibrary.Classes.Damage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLibrary.Classes.Items
+{
+    /// <summary>
+    /// Class meant for item, used to conflict damage
+    /// </summary>
+    public abstract class OffensiveItem : Item
+    {
+        public Damage.Damage DamageGive {  get; private set; }
+        public OffensiveItem(string name, string description, Damage.Damage damage) : base(name, description) 
+        {
+            DamageGive = damage;
+        }
+    }
+}
